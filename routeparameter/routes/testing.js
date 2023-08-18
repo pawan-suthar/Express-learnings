c4e7949dc5b15fa24cb10608a90b7027589b8e24
pawannn <pawanwhatsera@example.com>
2023-08-18 14:18:08 +0530

    ghhgjg

--- routeparameter/routes/testing.js
+++ routeparameter/routes/testing.js
@@ -1,8 +1,7 @@
-
 import express from 'express' 
 const router  = express.Router()
 
-// student routes
+// routing parameter examples
 
 router.get('/',(req,res)=>{
     res.send("test home page")
@@ -11,7 +10,6 @@ router.get('/product/:name/:id',(req,res)=>{
     const {name,id} = req.params
     res.send(`product is${name} and id:${id}`)
 })
-
 
 router.get('/destination/:from-:to',(req,res)=>{
     const {from,to} = req.params
@@ -22,5 +20,16 @@ router.get('/:year/:month/and/:day',(req,res)=>{
     const {month,day,year} = req.params
     res.send(`year ${year} month ${month} and day${day}`)
 })
+
+//  example routing parameter with regex
+
+router.get('/student/:id([0-9]{2})',(req,res)=>{ //([0-9]{2}) only 0-9 digits and must be 2 digit
+    const {id} = req.params
+    res.send(`student id is ${id} using regex`)
+})
+
+
+
+
 //  module.exports = router old
 export default router //new 
